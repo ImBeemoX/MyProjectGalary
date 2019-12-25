2c9cf536a65c8183303c0a69454709a1522f1311
Yichuan Wang <w369371@gmail.com>
2019-12-25 21:00:44 +1000

    add styling, add logo, modify css class

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import './App.css';
 import Project from './components/project';
 import reactlogo from './logos/React.png'
+import mylogo from './logos/Logo.JPG'
 
 function App() {
   return (
     <div className="App">
-      <header>
+       <img className = "icon" src = {mylogo}/>
+       <li className = "li"> 
+       <Project title = "Toy World" lang = "Swift" description = "Demo of a swift shopping app" image = {reactlogo}/>
        <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
        <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
        <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
        <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
        <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
-       <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
-       
-       
-      </header>
+       </li>
+
     </div>
   );
 }
--- src/components/project.js
+++ src/components/project.js
@@ -4,9 +4,9 @@ const Project = ({title, lang, description, image}) => {
       return (
         <div className = "Project">
         <img className = "Image" src={image} alt=""></img>
-        <h1>{title}</h1>
-        <h1>{lang}</h1>
-       <p>{description}</p>
+        <h1 style={{color: 'green', fontSize: 24}}>{title}</h1>
+        <h1 style={{color: 'yellow', fontSize: 16}}>{lang}</h1>
+        <p>{description}</p>
         
 
      </div>
