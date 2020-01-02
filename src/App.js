7be5e8d1600110a621a3911157f4d122355c0d17
Yichuan Wang <w369371@gmail.com>
2020-01-02 19:49:57 +1000

    add new projects and bugs fix

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
        <img className = "icon" src = {mylogo}/>
        <li className = "li"> 
        <Project title = "Toy World" lang = "Swift" description = "Demo of a swift shopping app" image = {reactlogo}/>
-       <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
-       <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
-       <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
-       <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
-       <Project title = "haha" lang = "React" description = "haha" image = {reactlogo}/>
+       <Project title = "Colour Switch" lang = "Swift" description = "A simple Swift game using SpriteKit" image = {reactlogo}/>
+       <Project title = "Scrape opgg" lang = "Python" description = "Scrape opgg to get information of players" image = {reactlogo}/>
+       <Project title = "Zombie game" lang = "p5.js" description = "A simple zombie game" image = {reactlogo}/>
+       <Project title = "Personal Expenses" lang = "Flutter" description = "An app to track your money transactions" image = {reactlogo}/>
+       <Project title = "Yelp" lang = "ReactNative" description = "An app used for searching restaurants nearby using Yelp API" image = {reactlogo}/>
        </li>
 
     </div>
--- src/components/project.js
+++ src/components/project.js
@@ -4,11 +4,10 @@ const Project = ({title, lang, description, image}) => {
       return (
         <div className = "Project">
         <img className = "Image" src={image} alt=""></img>
-        <h1 style={{color: 'green', fontSize: 24}}>{title}</h1>
+      <h1 style={{color: 'green', fontSize: 20}}>{title}</h1>
         <h1 style={{color: 'yellow', fontSize: 16}}>{lang}</h1>
         <p>{description}</p>
-        
-
+      
      </div>
       )
 }
