daa9d0d2d3eccd47475ea2d9bd768fe29a352335
Yichuan Wang <w369371@gmail.com>
2020-01-04 20:25:53 +1000

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import mylogo from './logos/Logo.JPG'
 import Projects from './components/projects'
 import Introduction from './components/Introduction'
+import Navigationbar from './components/navigationbar'
 
 function App() {
   return (
     <div className="App">
-       
+      <img className = "icon" src = {mylogo}/>
+       <Navigationbar/>
        <Introduction/>
        <Projects/>
     </div>
---
+++ src/components/navigationbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+const Navigationbar = () => {
+    return(
+       <div className = "navigationabr">
+           <button variant="outline-primary">Github</button>
+           <button variant="outline-primary">Contact me</button>
+           <button variant="outline-primary">Linkedin</button>
+       </div>
+    )
+}
+
+export default Navigationbar
